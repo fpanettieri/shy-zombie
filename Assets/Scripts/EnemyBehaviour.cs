4cbaac5253f091049a0c5e54535905dc2c6a8311
Fabio Renzo Panettieri <fpanettieri@gmail.com>
2012-11-14 15:58:15 -0300

    Line of sight algorithm working as expected

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour
+public class EnemyBehaviour : MonoBehaviour
 {
 	// inspector properties
+	public Transform enemy;
+	public Transform player;
+	
 	public BezierNavigator navigator;
 	public BezierPath path;
 	public float interval = 0;
 	public float skip = 0;
+	public float fov = 30;
+	public float viewDistance = 10;
 	
 	// internal state
 	private bool delayed;
 	private float delay;
 	private int counter;
 	
-	// auxiliar variables 
+	// auxiliar variables
 	private new Animation animation;
+	private RaycastHit hit;
+	
+	// FIXME: remove this
+	private LineRenderer line;
 	
 	public void Start()
 	{
@@ -25,10 +34,17 @@ public class EnemyController : MonoBehaviour
 		navigator.Navigate(path.GetCurvesArray());
 		animation = GetComponentInChildren<Animation>();
 		animation.Play("walk");
+		line = GetComponent<LineRenderer>();
 	}
 
 	public void Update ()
 	{
+		if(LineOfSight()){
+			Debug.Log("Player visible, distance: " + Vector3.Distance(enemy.position, player.position));
+			line.SetPosition(0, enemy.position);
+			line.SetPosition(1, player.position);
+		}
+		
 		if(delayed){
 			delay -= Time.deltaTime;
 			if(delay < 0){
@@ -52,5 +68,12 @@ public class EnemyController : MonoBehaviour
 	public void OnComplete()
 	{
 		navigator.Navigate(path.GetCurvesArray());
+	}
+	
+	private bool LineOfSight()
+	{
+		return Vector3.Angle(player.position - enemy.position, enemy.forward) < fov && 
+			Vector3.Distance(player.position, enemy.position) < viewDistance && 
+			Physics.Linecast(enemy.position, player.position, out hit) && hit.transform.CompareTag("Player");
 	}
 }
