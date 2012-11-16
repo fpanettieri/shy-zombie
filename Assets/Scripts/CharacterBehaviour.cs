77a6fc96853f88ee36ff06740835091466adb078
Fabio Renzo Panettieri <fpanettieri@gmail.com>
2012-11-16 16:50:50 -0300

    Improved Overworld

--- Assets/Scripts/CharacterBehaviour.cs
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -206,18 +206,4 @@ public class CharacterBehaviour : MonoBehaviour
 			brainzArr[i].SetActiveRecursively(true);
 		}
 	}
-	
-	public float pushPower = 2.0F;
-	public void OnControllerColliderHit(ControllerColliderHit hit)
-	{
-		Rigidbody body = hit.collider.attachedRigidbody;
-        if (body == null || body.isKinematic)
-            return;
-        
-        if (hit.moveDirection.y < -0.3F)
-            return;
-        
-        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-        body.velocity = pushDir * pushPower;
-	}
 }
